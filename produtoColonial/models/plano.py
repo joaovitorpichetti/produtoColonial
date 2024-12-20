3ef769c16c0f174f5ef3b462f8178a24e17e8d25
João Vitor Pichetti <joao.amaral.pichetti@gmail.com>
2024-12-19 21:22:17 -0300

    Add validação do dia, entre 01 a 31

--- produtoColonial/models/plano.py
+++ produtoColonial/models/plano.py
@@ -1,10 +1,11 @@
 from produtoColonial.models.base import BaseModel
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Plano(BaseModel):
     nome_plano = models.CharField(max_length=50, verbose_name="Nome do Plano", help_text="Informe o nome do Plano (Exemplo: Autoatendimento ou Cesta Juntos)")
     valor = models.FloatField(verbose_name="Valor da mensalidade", help_text="Informe o valor do plano, que deve ser paga mensalmente")
-    data_de_pagamento = models.IntegerField(verbose_name="Dia do Pagamento", help_text="Informe um dia para pagamento do plano (ex. dias: 5)")
+    data_de_pagamento = models.IntegerField(verbose_name="Dia do Pagamento", help_text="Informe um dia para pagamento do plano (entre 01 a 31)", validators=[MinValueValidator(1), MaxValueValidator(31)])
 
     class Meta:
         abstract = False
