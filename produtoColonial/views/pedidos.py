1a08961c78c8d2091c89e218a8b86dca596dbbf8
João Vitor Pichetti <joao.amaral.pichetti@gmail.com>
2025-01-05 21:14:30 -0300

    Arrumado View de edição do Pedido

--- produtoColonial/views/pedidos.py
+++ produtoColonial/views/pedidos.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from produtoColonial.models.pedido import Pedido
 from produtoColonial.forms.pedido_form import PedidoForm
+from produtoColonial.forms.pedido_form_status import PedidoFormStatus
 
 @login_required
 def pedidos(request):
@@ -24,17 +25,17 @@ def verPedido(request, pedido_id):
 def editarPedido(request, pedido_id):
     pedido = get_object_or_404(Pedido, pk=pedido_id)
     try:
-        if request.method == 'POST':
-            form = PedidoForm(request.POST, instance=pedido)
+        if request.method == "POST":
+            form = PedidoFormStatus(request.POST, files=request.FILES, instance=pedido)
             if form.is_valid():
                 form.save()
                 return redirect('aplicacao:pedidos')
         else:
-            form = PedidoForm(instance=pedido)
+            form = PedidoFormStatus(instance=pedido)
         conteudo = {
             "form": form,
-            "pedido_id": pedido_id
+            "pedido_id": pedido_id,
         }
         return render(request, 'produtoColonial/pedido_edit.html', conteudo)
     except:
-        return redirect('produtoColonial:pedidos')
+        return redirect(request, 'produtoColonial/pedidos.html')
